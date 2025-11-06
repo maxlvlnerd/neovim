91dc13445b5238aa9fa1044dd453da1dc86f92bf
Austin <160679820+maxlvlnerd@users.noreply.github.com>
2025-11-06 08:40:01 -0800

    shorthand plug syntax

--- init.fnl
+++ init.fnl
@@ -1,13 +1,13 @@
-(local {: plug : new-cmd : load-lazy : noremap} (require :boiler))
+(local {:plug p : new-cmd : load-lazy : noremap} (require :boiler))
 
 (set vim.g.mapleader " ")
 ;; plugins
 (do
-  (plug :mini.nvim {:config (fn []
-                              (let [config (require :mini.basics)]
-                                (config.setup)))})
-  (plug :nfnl {:ft :fennel})
-  (plug :typescript-tools.nvim {:opts {}})
+  (p :mini.nvim {:config (fn []
+                           (let [config (require :mini.basics)]
+                             (config.setup)))})
+  (p :nfnl {:ft :fennel})
+  (p :typescript-tools.nvim {:opts {}})
 
   (fn neorg-finder []
     (let [neorg (require :neorg)
@@ -17,37 +17,37 @@
       (icollect [_ v (ipairs workspace-files)]
         {:text (tostring (v:relative_to cwd false)) :file (tostring v)})))
 
-  (plug :snacks.nvim
-        {:keys [{1 :<leader>gg 2 #(Snacks.lazygit) :desc :Lazygit}
-                {1 :<leader><space> 2 #(Snacks.picker.smart)}
-                {1 :<leader>fg 2 #(Snacks.picker.grep)}
-                {1 "<leader>," 2 #(Snacks.picker.buffers)}
-                {1 :<leader>ld 2 #(Snacks.picker.diagnostics)}
-                {1 :<leader>no 2 #(Snacks.picker.neorg)}]
-         :lazy false
-         :priority 1000
-         :opts {:lazygit {:enabled true}
-                :picker {:sources {:neorg {:finder neorg-finder}}}}})
-  (plug :neorg
-        {:opts {:load {:core.defaults {}
-                       :core.concealer {}
-                       ; automatically create metadata
-                       :core.esupports.metagen {:config {:type :auto}}
-                       :external.interim-ls {:config {:completion_provider {:enable true
-                                                                            :documentation true}}}
-                       :core.completion {:config {:engine {:module_name :external.lsp-completion}}}
-                       :core.dirman {:config {:workspaces {:main "~/persist/logs/notes"}
-                                              :default_workspace :main}}}}})
-  (plug :conform.nvim
-        {:opts {:default_format_opts {:lsp_format :fallback}
-                :format_on_save {:timeout_ms 500}
-                :formatters_by_ft {:fennel [:fnlfmt]
-                                   :nix [:alejandra]
-                                   :typescript [:prettierd]}}})
-  (plug :blink.cmp
-        {:opts {:completion {:documentation {:auto_show true
-                                             :auto_show_delay_ms 500}}
-                :sources {:default [:lsp :path :buffer]}}}))
+  (p :snacks.nvim
+     {:keys [{1 :<leader>gg 2 #(Snacks.lazygit) :desc :Lazygit}
+             {1 :<leader><space> 2 #(Snacks.picker.smart)}
+             {1 :<leader>fg 2 #(Snacks.picker.grep)}
+             {1 "<leader>," 2 #(Snacks.picker.buffers)}
+             {1 :<leader>ld 2 #(Snacks.picker.diagnostics)}
+             {1 :<leader>no 2 #(Snacks.picker.neorg)}]
+      :lazy false
+      :priority 1000
+      :opts {:lazygit {:enabled true}
+             :picker {:sources {:neorg {:finder neorg-finder}}}}})
+  (p :neorg
+     {:opts {:load {:core.defaults {}
+                    :core.concealer {}
+                    ; automatically create metadata
+                    :core.esupports.metagen {:config {:type :auto}}
+                    :external.interim-ls {:config {:completion_provider {:enable true
+                                                                         :documentation true}}}
+                    :core.completion {:config {:engine {:module_name :external.lsp-completion}}}
+                    :core.dirman {:config {:workspaces {:main "~/persist/logs/notes"}
+                                           :default_workspace :main}}}}})
+  (p :conform.nvim
+     {:opts {:default_format_opts {:lsp_format :fallback}
+             :format_on_save {:timeout_ms 500}
+             :formatters_by_ft {:fennel [:fnlfmt]
+                                :nix [:alejandra]
+                                :typescript [:prettierd]}}})
+  (p :blink.cmp
+     {:opts {:completion {:documentation {:auto_show true
+                                          :auto_show_delay_ms 500}}
+             :sources {:default [:lsp :path :buffer]}}}))
 
 (load-lazy)
 (vim.lsp.enable :fennel_ls)
--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- [nfnl] init.fnl
- package.preload["boiler"] = package.preload["boiler"] or function(...) local function noremap(maps) for _, v in ipairs(maps) do vim.keymap.set((v.mode or ""), v.key, v.action, {silent = true, desc = v.desc}) end return nil end local function plug(name, opts) _G["plugin-specs"][name] = vim.tbl_extend("force", _G["plugin-specs"][name], opts) return nil end local function load_lazy() local lazy = require("lazy") local plugs do local tbl_26_ = {} local i_27_ = 0 for _, v in pairs(_G["plugin-specs"]) do local val_28_ = v if (nil ~= val_28_) then i_27_ = (i_27_ + 1) tbl_26_[i_27_] = val_28_ else end end plugs = tbl_26_ end return lazy.setup({checker = {enabled = true}, rocks = {enabled = false}, profiling = {loader = true, require = true}, spec = plugs}) end local function new_cmd(name, cb) return vim.api.nvim_create_user_command(name, cb, {}) end return {plug = plug, noremap = noremap, ["load-lazy"] = load_lazy, ["new-cmd"] = new_cmd} end local _local_2_ = require("boiler") local plug = _local_2_.plug local new_cmd = _local_2_["new-cmd"] local load_lazy = _local_2_["load-lazy"] local noremap = _local_2_.noremap vim.g.mapleader = " "
+ package.preload["boiler"] = package.preload["boiler"] or function(...) local function noremap(maps) for _, v in ipairs(maps) do vim.keymap.set((v.mode or ""), v.key, v.action, {silent = true, desc = v.desc}) end return nil end local function plug(name, opts) _G["plugin-specs"][name] = vim.tbl_extend("force", _G["plugin-specs"][name], opts) return nil end local function load_lazy() local lazy = require("lazy") local plugs do local tbl_26_ = {} local i_27_ = 0 for _, v in pairs(_G["plugin-specs"]) do local val_28_ = v if (nil ~= val_28_) then i_27_ = (i_27_ + 1) tbl_26_[i_27_] = val_28_ else end end plugs = tbl_26_ end return lazy.setup({checker = {enabled = true}, rocks = {enabled = false}, profiling = {loader = true, require = true}, spec = plugs}) end local function new_cmd(name, cb) return vim.api.nvim_create_user_command(name, cb, {}) end return {plug = plug, noremap = noremap, ["load-lazy"] = load_lazy, ["new-cmd"] = new_cmd} end local _local_2_ = require("boiler") local p = _local_2_.plug local new_cmd = _local_2_["new-cmd"] local load_lazy = _local_2_["load-lazy"] local noremap = _local_2_.noremap vim.g.mapleader = " "
 
 
 
  do
  local function _3_()
  local config = require("mini.basics")
- return config.setup() end plug("mini.nvim", {config = _3_})
- plug("nfnl", {ft = "fennel"})
- plug("typescript-tools.nvim", {opts = {}})
+ return config.setup() end p("mini.nvim", {config = _3_})
+ p("nfnl", {ft = "fennel"})
+ p("typescript-tools.nvim", {opts = {}})
 
  local function neorg_finder()
  local neorg = require("neorg")
@@ -24,28 +24,28 @@
  local function _8_() return Snacks.picker.grep() end
  local function _9_() return Snacks.picker.buffers() end
  local function _10_() return Snacks.picker.diagnostics() end
- local function _11_() return Snacks.picker.neorg() end plug("snacks.nvim", {keys = {{"<leader>gg", _6_, desc = "Lazygit"}, {"<leader><space>", _7_}, {"<leader>fg", _8_}, {"<leader>,", _9_}, {"<leader>ld", _10_}, {"<leader>no", _11_}}, priority = 1000, opts = {lazygit = {enabled = true}, picker = {sources = {neorg = {finder = neorg_finder}}}}, lazy = false})
+ local function _11_() return Snacks.picker.neorg() end p("snacks.nvim", {keys = {{"<leader>gg", _6_, desc = "Lazygit"}, {"<leader><space>", _7_}, {"<leader>fg", _8_}, {"<leader>,", _9_}, {"<leader>ld", _10_}, {"<leader>no", _11_}}, priority = 1000, opts = {lazygit = {enabled = true}, picker = {sources = {neorg = {finder = neorg_finder}}}}, lazy = false})
 
 
 
 
- plug("neorg", {opts = {load = {["core.defaults"] = {}, ["core.concealer"] = {}, ["core.esupports.metagen"] = {config = {type = "auto"}}, ["external.interim-ls"] = {config = {completion_provider = {enable = true, documentation = true}}}, ["core.completion"] = {config = {engine = {module_name = "external.lsp-completion"}}}, ["core.dirman"] = {config = {workspaces = {main = "~/persist/logs/notes"}, default_workspace = "main"}}}}})
-
-
-
-
-
-
-
-
-
- plug("conform.nvim", {opts = {default_format_opts = {lsp_format = "fallback"}, format_on_save = {timeout_ms = 500}, formatters_by_ft = {fennel = {"fnlfmt"}, nix = {"alejandra"}, typescript = {"prettierd"}}}})
+ p("neorg", {opts = {load = {["core.defaults"] = {}, ["core.concealer"] = {}, ["core.esupports.metagen"] = {config = {type = "auto"}}, ["external.interim-ls"] = {config = {completion_provider = {enable = true, documentation = true}}}, ["core.completion"] = {config = {engine = {module_name = "external.lsp-completion"}}}, ["core.dirman"] = {config = {workspaces = {main = "~/persist/logs/notes"}, default_workspace = "main"}}}}})
 
 
 
 
 
- plug("blink.cmp", {opts = {completion = {documentation = {auto_show = true, auto_show_delay_ms = 500}}, sources = {default = {"lsp", "path", "buffer"}}}}) end
+
+
+
+
+ p("conform.nvim", {opts = {default_format_opts = {lsp_format = "fallback"}, format_on_save = {timeout_ms = 500}, formatters_by_ft = {fennel = {"fnlfmt"}, nix = {"alejandra"}, typescript = {"prettierd"}}}})
+
+
+
+
+
+ p("blink.cmp", {opts = {completion = {documentation = {auto_show = true, auto_show_delay_ms = 500}}, sources = {default = {"lsp", "path", "buffer"}}}}) end
 
 
 
